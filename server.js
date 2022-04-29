142d3e0e32ecf06b7970a2b0fbd90132a9b62464
NeilVakharia45 <neildvakharia@gmail.com>
2022-04-29 18:46:18 -0400

    Added summary

--- server.js
+++ server.js
@@ -58,32 +58,32 @@ function coinFlip() {
   }
 
 // Define all the endpoints
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-    res.statusCode = 200;
-    // Respond with status message "OK"
-    res.statusMessage = 'OK';
-    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+ ' ' +res.statusMessage)
+app.get('/app/', (req,res) => {
+  // Respond with status 200
+  res.statusCode = 200;
+  // Respond with status message "OK"
+  res.statusMessage = 'OK';
+  res.writeHead(res.statusCode, {'Content-Type' : 'text/plain'});
+  res.end(res.statusCode + ' ' + res.statusMessage)
 });
 
-app.get('/app/flip', (req, res) => {
-    res.send({flip: coinFlip()})
+app.get('/app/flip', (req,res) => {
+  res.send({flip: coinFlip()})
 });
 
-app.get('/app/flips/:number', (req, res) => {
-    res.send(coinFlips(req.params.number))
+app.get('/app/flips/:number', (req,res) => {
+  res.send(coinFlips(req.params.number))
 });
 
-app.get('app/flip/call/heads', (req, res) => {
-    res.status(200).json(flipACoin("heads"))
+app.get('/app/flip/call/heads', (req,res) => {
+  res.status(200).json(flipACoin("heads"))
 });
 
-app.get('app/flip/call/tails', (req, res) => {
-    res.status(200).json(flipACoin("tails"))
+app.get('/app/flip/call/tails', (req,res) => {
+  res.status(200).json(flipACoin("tails"))
 });
 
-// Default response
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+// Default response for any other request
+app.use(function(req,res) {
+  res.status(404).send('404 NOT FOUND')
 });
